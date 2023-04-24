c023c49fe27689f2f038fca08c362510124a0a00
fblochwitz <felix.blochwitz@gmail.com>
2023-04-24 17:18:19 +0200

    formatting

--- calculator.js
+++ calculator.js
@@ -1,28 +1,26 @@
 let btns = document.querySelectorAll("button");
 let display = document.querySelector(".display");
-let displayTop = document.querySelector("#display-top")
-let displayBottom = document.querySelector("#display-bottom")
+let displayTop = document.querySelector("#display-top");
+let displayBottom = document.querySelector("#display-bottom");
 
 let ansResult = null;
 
-btns.forEach(button => button.addEventListener("click", function(e) {
-  if (e.target.id === "del") {
-    clearDisplay();
-  }
-  else if (e.target.textContent === "="){
-    evaluate();
-  }
-  else if (e.target.textContent === "ANS") {
-    console.log(ansResult);
-    if ((displayTop.textContent.length + ansResult.toString().length) <= 15) {
-      writeToDisplay(ansResult);
+btns.forEach((button) =>
+  button.addEventListener("click", function (e) {
+    if (e.target.id === "del") {
+      clearDisplay();
+    } else if (e.target.textContent === "=") {
+      evaluate();
+    } else if (e.target.textContent === "ANS") {
+      if (displayTop.textContent.length + ansResult.toString().length <= 15) {
+        writeToDisplay(ansResult);
+      }
+    } else if (displayTop.textContent.length <= 15) {
+      let input = e.target.textContent;
+      writeToDisplay(input);
     }
-  }
-  else if (displayTop.textContent.length <= 15){
-  let input = e.target.textContent;
-  writeToDisplay(input);
-  }
-}));
+  })
+);
 
 function writeToDisplay(x) {
   displayTop.textContent += x;
@@ -44,14 +42,12 @@ function evaluate() {
 
 function saveResultToAns(lastResult) {
   ansResult = lastResult;
-} 
-
+}
 
 function setDisplayFontSize() {
   let fontSize = displayTop.offsetHeight - 1;
   displayTop.style.fontSize = `${fontSize}px`;
   displayBottom.style.fontSize = `${fontSize}px`;
 }
-
 
 setDisplayFontSize();
